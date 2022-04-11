09c57e3c70938a7cce577f04db79fc001ed0baf5
Dmytro <sunsn4ke@gmail.com>
2022-04-11 19:13:42 +0300

    clear code before start

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,8 @@
-import React, {StrictMode} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import {Header} from './App';
 
 ReactDOM.render(
-  <StrictMode>
-    <App/>
-  </StrictMode>,
+  
   document.getElementById('root')
 );
