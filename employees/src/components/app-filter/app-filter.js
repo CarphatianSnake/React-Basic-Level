f5017d3221790d42b78353c9d6eb6cca34a12698
Dmytro <sunsn4ke@gmail.com>
2022-04-27 17:28:49 +0300

    make filters solo

--- employees/src/components/app-filter/app-filter.js
+++ employees/src/components/app-filter/app-filter.js
@@ -1,25 +1,45 @@
+import { Component } from 'react';
 import './app-filter.css';
 
-const AppFilter = () => {
-  return (
-    <div className="btn-group">
-      <button
-        className="btn btn-light"
-        type="button">
-          Все сотрудники
-      </button>
-      <button
-        className="btn btn-outline-light"
-        type="button">
-          Сотрудники на повышение
-      </button>
-      <button
-        className="btn btn-outline-light"
-        type="button">
-          З/П больше 1000%
-      </button>
-    </div>
-  )
+class AppFilter extends Component {
+
+  btnChangeClass = (e) => {
+    document.querySelectorAll('button').forEach(item => {
+      item.classList.remove('btn-light');
+      item.classList.add('btn-outline-light');
+    })
+    e.target.classList.remove('btn-outline-light');
+    e.target.classList.add('btn-light');
+  }
+
+  render() {
+    const {onFilter} = this.props;
+    return (
+      <div className="btn-group">
+        <button
+          className="btn btn-light"
+          type="button"
+          data-button="all"
+          onClick={(e) => {this.btnChangeClass(e); onFilter(e.target.getAttribute('data-button'))}}>
+            Все сотрудники
+        </button>
+        <button
+          className="btn btn-outline-light"
+          type="button"
+          data-button="toPromotion"
+          onClick={(e) => {this.btnChangeClass(e); onFilter(e.target.getAttribute('data-button'))}}>
+            Сотрудники на повышение
+        </button>
+        <button
+          className="btn btn-outline-light"
+          type="button"
+          data-button="salaryOver1000"
+          onClick={(e) => {this.btnChangeClass(e); onFilter(e.target.getAttribute('data-button'))}}>
+            З/П больше 1000%
+        </button>
+      </div>
+    )
+  }
 }
 
 export default AppFilter;
--- employees/src/components/app/app.js
+++ employees/src/components/app/app.js
@@ -14,7 +14,9 @@ class App extends Component {
         {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
         {name: 'Alex S.', salary: 3000, increase: false, rise: false, id: 2},
         {name: 'Dmytro M.', salary: 5000, increase: true, rise: false, id: 3}
-      ]
+      ],
+      term: '',
+      action: 'all'
     }
   }
 
@@ -55,9 +57,40 @@ class App extends Component {
     }))
   }
 
+  searchEmp = (items, term) => {
+    const action = this.state.action;
+    const filteredItems = items.filter(item => {
+      if (action === 'toPromotion' && item.rise) return item
+      if (action === 'salaryOver1000' && item.salary > 1000) return item
+      if (action === 'all') return item
+      return null;
+    })
+
+    if (term.length === 0) {
+      return filteredItems;
+    }
+    return filteredItems.filter(item => {
+      return item.name.toLowerCase().indexOf(term) > -1
+    })
+  }
+
+  onUpdateSearch = (term) => {
+    this.setState({
+      term: term
+    })
+  }
+
+  onFilter = (action) => {
+    this.setState({
+      action: action
+    })
+  }
+
   render() {
+    const {data, term} = this.state;
     const employees = this.state.data.length,
           increased = this.state.data.filter(item => item.increase).length;
+    const visibleData = this.searchEmp(data, term);
     return (
       <div className="app">
         <AppInfo
@@ -66,16 +99,21 @@ class App extends Component {
         />
   
         <div className="search-panel">
-          <SearchPanel/>
-          <AppFilter/>
+          <SearchPanel
+              onUpdateSearch={this.onUpdateSearch}
+          />
+          <AppFilter
+              onFilter={this.onFilter}
+          />
         </div>
   
         <EmpolyeesList
-              data={this.state.data}
+              data={visibleData}
               onDelete={this.deleteItem}
               onToggleProp={this.onToggleProp}
               />
-        <EmployeesAddForm onAdd={this.onAdd}/>
+        <EmployeesAddForm
+              onAdd={this.onAdd}/>
       </div>
     );
   }
